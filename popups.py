71073a18e5f3a96c0cd8158f14141c1bc01182d9
Long <hoanglongmit@gmail.com>
2019-09-13 02:43:45 +0700

    Update README.md
    bruh

--- popups.py
+++ popups.py
@@ -4,15 +4,14 @@ from tkinter import ttk
 def commingsoon():
     popup = tk.Tk()
     popup.wm_title('.')
-    ttk.Label(popup, text="Comming Soon :)").pack()
-    ttk.Button(popup, text="OK, cool", command = popup.destroy ).pack()
+    ttk.Label(popup, text="Comming Soon maybe").pack()
+    ttk.Button(popup, text="OK", command = popup.destroy ).pack()
        
 
 def about_dialog():
     popup = tk.Tk()
     popup.wm_title('About')
-    ttk.Label(popup, text="Where's My Water Level Creator v0.0.1 pre-alpha release").pack()
-    ttk.Label(popup, text='Brought to you by Long Nguyễn').pack()
-    ttk.Label(popup, text="note that I have no association with WMW's creators or whosoever").pack()
-    ttk.Button(popup, text="Close", command = popup.destroy ).pack()
+    ttk.Label(popup, text="Hai").pack()
+    ttk.Label(popup, text="Enjoying the program?").pack()
+    ttk.Button(popup, text="Eh", command = popup.destroy ).pack()
     
